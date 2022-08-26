7cb9bf9004e9bb4e1cce7ce96e9efafddfa2d905
tal-sitton <tal.sitton04@gmail.com>
2022-08-26 09:08:37 +0000

    chaged get_running_loop to get_event_loop because sometimes there is no running loop

--- main.py
+++ main.py
@@ -285,6 +285,9 @@ def add_manually():
     """
     global user_scores, calculated
     session = requests.Session()
+    game = "$$$$$$$$$$$$$$$"
+    while game != '':
+
     game = input("whats the name of the game? ENTER to main menu\n")
     rating = input("how much do u rate the game from 1 to 5\n")
 
@@ -293,8 +296,7 @@ def add_manually():
         print("INVALID INPUT")
         rating = input("how much do u rate the game from 1 to 5\n")
 
-    while game != '':
-        urls = asyncio.get_running_loop().run_until_complete(get_platforms(game, True))
+        urls = asyncio.get_event_loop().run_until_complete(get_platforms(game, True))
         ans = 'n'
         i = 0
         while ans != 'y':
@@ -317,7 +319,6 @@ def add_manually():
             calculated = False
         else:
             print(ans)
-        game = input("whats the name of the game? ENTER to main menu\n")
     clear_score()
 
 
@@ -341,7 +342,7 @@ def manager():
 
         elif todo == "recommended games" or todo == "r":
             if not calculated:
-                asyncio.get_running_loop().run_until_complete(prog_games_rating())
+                asyncio.get_event_loop().run_until_complete(prog_games_rating())
             print(prog_scores)
             input("continue? press Enter")
 
